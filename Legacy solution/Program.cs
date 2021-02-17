ee9dfc85deac6e3ea978c15817a0145c99f2f1b9
laurabirchx <57993207+laurabirchx@users.noreply.github.com>
2021-02-17 09:32:36 +0100

    Add project files.

---
+++ Legacy solution/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Legacy_solution
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var ecs = new ECS(28);
+
+            ecs.Regulate();
+
+            ecs.SetThreshold(20);
+
+            ecs.Regulate();
+        }
+    }
+}
